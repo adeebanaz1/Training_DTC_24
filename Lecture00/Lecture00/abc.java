3d721ec657271fe5dae81c8da9edf29dea928335
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-03 00:42:09 +0530

    pattern printing

---
+++ Lecture00/Lecture00/abc.java
@@ -0,0 +1,26 @@
+public class abc {
+    public static void main(String[]args){
+        // int i = 1;
+        // while (i<=10)
+        //     System.out.println(i++);
+        
+    // int i = 10;
+    // do {
+    //     System.out.println(i--);    
+    // }
+    // while(i>=1);
+
+    // int n = 4;
+    // for(int i=1; i<=n; i++)
+    //     System.out.println("*");
+
+    // int n = 5;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++)
+    //     System.out.print("*");
+    // System.out.println();
+    // }
+
+    
+    }
+}
---
+++ pattern_printing.java
@@ -0,0 +1,7 @@
+public class pattern_printing{
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=0;i<n;i++)
+        System.out.println("*");
+    }
+}
