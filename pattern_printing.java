ce9cf776eb89b89f795bd84c65c97068f402e432
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-03 01:21:30 +0530

    Pattern Matching

--- pattern_printing.java
+++ pattern_printing.java
@@ -1,7 +1,16 @@
 public class pattern_printing{
     public static void main(String[] args) {
         int n = 5;
-        for (int i=0;i<n;i++)
-        System.out.println("*");
+        // for (int i=0;i<n;i++)
+        // System.out.println("*");
+        // System.out.print("*");
+        for (int i=0;i<n;i++){
+        //     for (int j=0;j<n;j++)
+        //     System.out.print("*");
+        // System.out.println();
+        for (int j=0;j<=i;j++)
+            System.out.print("*");
+        System.out.println();
+        }
     }
 }
