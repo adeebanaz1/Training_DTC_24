16f1f172361b2b7a4541b2dad9a2adb6d1e35c73
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-03 15:19:56 +0530

    Array

--- pattern_printing.java
+++ pattern_printing.java
@@ -74,17 +74,21 @@ public class pattern_printing{
         // }
         
         // Question 8
-        for(int i=1;i<=n;i++){
-            for(int j=1; j<=n;j++){
-                if(i==j || i+j==n+1)
-                System.out.print("*");
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1; j<=n;j++){
+        //         if(i==j || i+j==n+1)
+        //         System.out.print("*");
 
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        
         }
+
         }
-    }
+    
 
