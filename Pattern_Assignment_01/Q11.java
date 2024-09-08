9dd361e23f1fba1b0917d21a781a8b32a2f56e29
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-09 00:11:48 +0530

    Tree with space

---
+++ Pattern_Assignment_01/Q11.java
@@ -0,0 +1,18 @@
+package Pattern_Assignment_01;
+
+public class Q11 {
+    public static void main(String[]args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+        
+        System.out.println();
+        }
+    }
+    
+}
---
+++ Pattern_Assignment_01/Q13.java
@@ -0,0 +1,5 @@
+package Pattern_Assignment_01;
+
+public class Q13 {
+    
+}
