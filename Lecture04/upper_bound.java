1cad6cce296f22a18f394f144baec8db30e8ed27
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-10 12:37:04 +0530

    Upper Bound

--- Lecture04/upper_bound.java
+++ Lecture04/upper_bound.java
@@ -20,7 +20,7 @@ public class upper_bound {
     return ans;
 }
     public static void main(String[] args){
-        int[] arr={1,2,2,2,3,3,4,4,4,4,4,4,4};
+        int[] arr={1,2,2,2,3,3,4,4,4,4,4,4,4,5};
         System.out.println(upperbound(arr, 4));    
     
 }
