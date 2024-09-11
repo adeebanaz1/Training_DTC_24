7697dfaa2a0d9c5f7257fefaf5d909052b90df60
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-12 00:36:56 +0530

    Tree with exclamation mark

---
+++ Lecture04/occurences.java
@@ -0,0 +1,12 @@
+package Lecture04;
+
+public class occurences {
+    static void display(int[] arr){
+        for(int i: arr)
+        System.out.print(i+" ");
+    }
+
+    public static void main(String[] args){
+        int[] arr = {4,2,1,4,2,3,3,1,5};
+    }
+}
--- Pattern_Assignment_01/Q12.java
+++ Pattern_Assignment_01/Q12.java
@@ -4,13 +4,17 @@ public class Q12 {
     public static void main(String[]args){
         int n=5;
         for(int i=1;i<=n;i++){
-            for(int k=1;k<=n-i;k++){
-                System.out.print("   ");
+            for(int k=n;k>i;k--){
+                System.out.print("  ");
             }
-            for(int j=1;j<=i*2-1;j++){
-                System.out.print(" * ");
-            }
-        
+            for(int j=1;j<=2*i-1;j++){
+                if(j%2==0){
+                    System.out.print(" !");
+                }
+                else{
+                    System.out.print(" *");
+                }
+            }       
         System.out.println();
         }
     }
