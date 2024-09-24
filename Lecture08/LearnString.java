4f336b6e8461c8f8aba6477b8cbc053135562c82
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-24 15:30:33 +0530

    Concat case

--- Lecture08/LearnString.java
+++ Lecture08/LearnString.java
@@ -28,17 +28,28 @@ public class LearnString {
 
     //Part 5
 
-    String s0 = "Phone";
-    int[] i0 = {65,66,67};
-    char[] c0 = {'a','b','c'};
-    byte[] b0 = new byte[127];
+    // String s0 = "Phone";
+    // int[] i0 = {65,66,67};
+    // char[] c0 = {'a','b','c'};
+    // byte[] b0 = new byte[127];
 
-    for(byte i=0; i<b0.length;i++)
-        b0[i]=i;
+    // for(byte i=0; i<b0.length;i++)
+    //     b0[i]=i;
 
-    String s1 = new String(b0);
-    for(int i=0; i<s1.length();i++)
-    System.out.println(s1.charAt(i));
+    // String s1 = new String(b0);
+    // for(int i=0; i<s1.length();i++)
+    // System.out.println(s1.charAt(i));
+
+    //Part 6
+
+    String s1 = "Laptop";
+    String s2 = "Phone";
+    String s3 = s1+s2;
+    //String s3 = s1.concat(s2);
+    //System.out.println(s1.equals(s2));
+    //System.out.println(s1.equalsIgnoreCase(s2));
+    //System.out.println(s1.toCharArray());
+    System.out.println(10+20+30+40+s3+10+20);
     
     }
 }    
