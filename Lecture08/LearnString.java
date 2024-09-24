9324c6774a33932598321145fd24d959787262ac
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-24 12:04:53 +0530

    Part 2

--- Lecture08/LearnString.java
+++ Lecture08/LearnString.java
@@ -2,13 +2,20 @@ package Lecture08;
 
 public class LearnString {
     public static void main(String[] args) {
-        String s1 = "Hello";
-        String s2 = new String();
-        s2 = "Hello";
-        String s3 = new String("Hello");
-        String s4 = "Hello";
-        String s5 = new String("Hello");
-         System.out.println(s3==s5);
-    }
+    //     Part 1
+    //     String s1 = "Hello";
+    //     String s2 = new String();
+    //     s2 = "Hello";
+    //     String s3 = new String("Hello");
+    //     String s4 = "Hello";
+    //     String s5 = new String("Hello");
+    //      System.out.println(s3==s5);
+
+    // Part 2
+    String s1 = "Hello";
+    String s2 = "Hello";
+    s2+="2";
+    System.out.println(s2);
+     }
     
 }
