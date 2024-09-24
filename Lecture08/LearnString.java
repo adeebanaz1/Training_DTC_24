4614df81cde01c3210b3d7a7b3754865f118d06c
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-24 15:11:09 +0530

    Part 5

--- Lecture08/LearnString.java
+++ Lecture08/LearnString.java
@@ -18,11 +18,23 @@ public class LearnString {
     // System.out.println(s1+" "+s2);
 
     // Part 3
-    String s1 = new String("He");
-    String s2 = new String ("hello");
-    for(int i=0;i<s2.length();i++)
+    // String s1 = new String("He");
+    // String s2 = new String ("hello");
+    // for(int i=0;i<s2.length();i++)
         //System.out.println(s1.compareTo(s2));
-    System.out.println(s1.substring(i));
+    //System.out.println(s1.substring(i));
+
+    //Part 4 longest occuring and palindrome
+
+    //Part 5
+
+    String s0 = "Phone";
+    int[] i0 = {65,66,67};
+    char[] c0 = {'a','b','c'};
+    byte[] b0 = {65,70,71,72};
+
+    String s1 = new String(b0,1,2);
+    System.out.println(s1);
     
     }
 }    
