50335b40bb3a90fd917b2bcaf34b139f5d70769f
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-27 10:57:59 +0530

    Input

--- Lecture09/TwoDArrays.java
+++ Lecture09/TwoDArrays.java
@@ -3,8 +3,8 @@ package Lecture09;
 public class TwoDArrays {
     public static void main(String[] args) {
         int[][] arr = new int[3][4];
-        System.out.println(arr[1]);
-        
-    }
-    
+        arr[0][3] = 4;
+        System.out.println(arr[1]);   
+    }   
 }
+ 
---
+++ Lecture09/input.java
@@ -0,0 +1,20 @@
+package Lecture09;
+
+public class input {
+    public static void main(String[] args){
+        int n =1;
+        int[][] arr = new int[3][4];
+        for(int i = 0; i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+            arr[i][j]=n;
+            n++;
+        }
+    }
+        for(int i = 0; i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+            System.out.print(arr[i][j]+" ");
+        }
+        System.out.println();
+        }
+    }
+}
