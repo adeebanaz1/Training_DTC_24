5b16b375495267869e5606676b7b53368ae4b975
Adeeba-Naz <justcreated.01@gmail.com>
2024-09-27 11:07:10 +0530

    Input using formula

--- Lecture09/input.java
+++ Lecture09/input.java
@@ -2,12 +2,12 @@ package Lecture09;
 
 public class input {
     public static void main(String[] args){
-        int n =1;
+        //int n =1;
         int[][] arr = new int[3][4];
         for(int i = 0; i<arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
-            arr[i][j]=n;
-            n++;
+            arr[i][j]= 4*i+(j+1); or // arr[i][j] = n;
+            //n++;
         }
     }
         for(int i = 0; i<arr.length;i++){
