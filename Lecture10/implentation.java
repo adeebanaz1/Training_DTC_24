b82008d6fbbe09fbefae24eb840b4243bc8cf069
Adeeba-Naz <justcreated.01@gmail.com>
2024-10-16 11:44:04 +0530

    Integers(-128 to 127 = true) , otherwise use .equals

--- Lecture10/implentation.java
+++ Lecture10/implentation.java
@@ -11,15 +11,24 @@ public class implentation {
 
         //String Interning
         // String s1 = "Laptop";
-        // String s2 = "pen";
+        // String s2 = "Laptop";
+        // s2 = "pen";
 
         //Autoboxing and Unboxing
 
-        int n = 10;
-        Integer m = n; //Autoboxing
-        int o = m; // Unboxing
-        System.out.println(m);
+        // int n = 10;
+        // Integer m = n; //Autoboxing
+        // int o = m; // Unboxing
+        // System.out.println(m);
 
+        Integer a1 = 83;
+        Integer a2 = 66;
+        Integer a3 = 66;
+        Integer a4 = 258;
+        Integer a5 = 258;
+        Integer a6 = -128;
+        Integer a7 = -128;
+        System.out.println(a4.equals(a5));
     }
     
 }
