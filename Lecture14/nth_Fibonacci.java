33a9d2ee29583b8d8cb3c0244af4e3901d416729
Adeeba-Naz <justcreated.01@gmail.com>
2024-11-29 12:38:19 +0530

    With HashMap

--- Lecture14/nth_Fibonacci.java
+++ Lecture14/nth_Fibonacci.java
@@ -1,21 +1,30 @@
 package Lecture14;
 
+import java.util.HashMap;
+
 public class nth_Fibonacci {
 
-    public static int fibo(int n){
+    public static int fibo(int n, HashMap<Integer,Integer> brain){
 
         if(n==0 || n==1){
             return n;
         }
 
-        int chhotaAns1 = (n-1);
-        int chhotaAns2 = (n-2);
+        if(brain.containsKey(n)){
+            return brain.get(n);
+        }
+        System.out.println("N is" + n);
+        int chhotaAns1 = fibo(n -1, brain);
+        int chhotaAns2 = fibo(n-2, brain);
 
         int meraAns = chhotaAns1 + chhotaAns2;
+        brain.put(n , meraAns);
         return meraAns;
     }
+
         public static void main(String[] args) {
-            System.out.println(fibo(5));
+            HashMap<Integer , Integer> brain = new HashMap<>();
+            System.out.println(fibo(10,brain));
             
         }
 }
